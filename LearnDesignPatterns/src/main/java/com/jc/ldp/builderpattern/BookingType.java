1c6a3b567b363925a58aecac4c4a17a849b3513d
s.hegde <s.hegde@devon.nl>
2018-08-01 15:08:50 +0530

    adding package back

---
+++ LearnDesignPatterns/src/main/java/com/jc/ldp/builderpattern/BookingType.java
@@ -0,0 +1,7 @@
+package com.jc.ldp.builderpattern;
+
+public enum BookingType {
+	
+	INDIVIDUAL, COACHING_SESSION, LIFETIME_MEMBER_GROUP; 
+
+}
---
+++ LearnDesignPatterns/src/main/java/com/jc/ldp/builderpattern/PersonalNotification.java
@@ -0,0 +1,7 @@
+package com.jc.ldp.builderpattern;
+
+public abstract class PersonalNotification {
+	
+	abstract void sendNotification(BookingContext b);
+
+}
