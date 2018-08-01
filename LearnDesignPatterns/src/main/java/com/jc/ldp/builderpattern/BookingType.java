b535523d5d6bc50c66e842c04e41ce1234181c96
s.hegde <s.hegde@devon.nl>
2018-08-01 14:56:32 +0530

    Deleting previous package structure

--- LearnDesignPatterns/src/main/java/com/jc/ldp/builderpattern/BookingType.java
+++
@@ -1,7 +0,0 @@
-package com.jc.ldp.builderpattern;
-
-public enum BookingType {
-	
-	INDIVIDUAL, COACHING_SESSION, LIFETIME_MEMBER_GROUP; 
-
-}
--- LearnDesignPatterns/src/main/java/com/jc/ldp/builderpattern/PersonalNotification.java
+++
@@ -1,7 +0,0 @@
-package com.jc.ldp.builderpattern;
-
-public abstract class PersonalNotification {
-	
-	abstract void sendNotification(BookingContext b);
-
-}
