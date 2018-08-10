7fc6f837338b92f0a5de9f0a3b4843dbf95233e3
akshay <akshay@Win7PC>
2018-08-11 00:56:16 +0530

    Run changes

--- LearnDesignPatterns/src/main/java/com/designpatterns/creational/builder/ManageBookings.java
+++ LearnDesignPatterns/src/main/java/com/designpatterns/creational/builder/ManageBookings.java
@@ -3,7 +3,7 @@ package com.designpatterns.creational.builder;
 public class ManageBookings {
 
 	public static void main(String[] args) {
-		BookingContext bc = new BookingContext("Manoj", BookingType.LIFETIME_MEMBER_GROUP);
+		BookingContext bc = new BookingContext("Seetaram", BookingType.COACHING_SESSION);
 
 		NotificationBuilder nb = new NotificationBuilder();
 		nb.buildNotification(bc).Notify();
